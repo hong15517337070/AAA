172a8f7aea07666cac14d746fc592324d47804c4
wumingxiaobei-hong <70378546+wumingxiaobei-hong@users.noreply.github.com>
2020-10-29 00:49:03 +0800

    Initial commit

---
+++ app/src/main/java/com/example/aaa/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.aaa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        android.util.Log.i(TAG, "onCreate: 111");
+    }
+}
